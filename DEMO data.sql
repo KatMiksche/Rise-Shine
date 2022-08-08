fbfeebadb2a51d27d634f63bc5bab62180453156
KatMiksche <katarina.miksche@gmail.com>
2022-08-09 00:59:19 +0100

    Update DEMO data.sql

--- DEMO data.sql
+++ DEMO data.sql
@@ -1,13 +1,16 @@
 use riseshine;
 
-insert into portfolio (name) values ('DEMO PORTFOLIO');
+insert into portfolio (name) values ('DEMOx');
 
-update value set Date='2022-06-01' where portfolioid=(select portfolioid from portfolio where name='DEMO PORTFOLIO');
+set @id=(select portfolioid from portfolio where name='DEMOx');
+update value set Date=DATE_SUB(curdate(), INTERVAL 60 DAY) where portfolioid=@id;
 
 insert into hold (ticker, volume, portfolioid) values 
-('IBM',100,(select portfolioid from portfolio where name='DEMO PORTFOLIO')),
-('MSFT',50,(select portfolioid from portfolio where name='DEMO PORTFOLIO')),
-('AAPL',10,(select portfolioid from portfolio where name='DEMO PORTFOLIO'));
+('IBM',100,@id),
+('MSFT',50,@id),
+('AAPL',10,@id);
 
 SET @var  = (select currentwallet from wallet order by date desc limit 1) ;
 insert into wallet (currentwallet, valuechange, description) values (10000+@var,10000,'DEMO MONEY');
+
+update portfolio set name='DEMO PORTFOLIO' where name='DEMOx';
