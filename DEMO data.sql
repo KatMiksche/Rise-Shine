32a87aea9ff6ee8621188add5ad647f0d5a73d6f
KatMiksche <katarina.miksche@gmail.com>
2022-08-08 21:36:19 +0100

    SQL demo data repair

--- DEMO data.sql
+++ DEMO data.sql
@@ -2,11 +2,12 @@ use riseshine;
 
 insert into portfolio (name) values ('DEMO PORTFOLIO');
 
-update value set Date='2022-01-01' where portfolioid=(select portfolioid from portfolio where name='DEMO PORTFOLIO');
+update value set Date='2022-06-01' where portfolioid=(select portfolioid from portfolio where name='DEMO PORTFOLIO');
 
 insert into hold (ticker, volume, portfolioid) values 
 ('IBM',100,(select portfolioid from portfolio where name='DEMO PORTFOLIO')),
 ('MSFT',50,(select portfolioid from portfolio where name='DEMO PORTFOLIO')),
 ('AAPL',10,(select portfolioid from portfolio where name='DEMO PORTFOLIO'));
 
-insert into wallet (currentwallet, valuechange, description) values (10000,10000,'DEMO MONEY');
+SET @var  = (select currentwallet from wallet order by date desc limit 1) ;
+insert into wallet (currentwallet, valuechange, description) values (10000+@var,10000,'DEMO MONEY');
